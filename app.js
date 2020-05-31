26e6feeb401e0fcaf11e42fff4c20e492923fe14
nitinpal07 <nitinluck0123@gmail.com>
2020-05-31 21:16:33 +0530

    add admin panel

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ mongoose.connect(CONNECTION_URI, {useNewUrlParser : true})
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin',adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Shubham Tyagi's Blog" });
+  res.render('users/index', { title: "Shubham Tyagi's Blog" });
 });
 
 router.get('/travel', function(req, res, next) {
-  res.render('travel', { title: "Travel | Shubham Tyagi's Blog" });
+  res.render('users/travel', { title: "Travel | Shubham Tyagi's Blog" });
 })
 router.get('/fashion', function(req, res, next) {
-  res.render('fashion', { title: "Fasthion | Shubham Tyagi's Blog" });
+  res.render('users/fashion', { title: "Fasthion | Shubham Tyagi's Blog" });
 })
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: "About | Shubham Tyagi's Blog" });
+  res.render('users/about', { title: "About | Shubham Tyagi's Blog" });
 })
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: "Contact | Shubham Tyagi's Blog" });
+  res.render('users/contact', { title: "Contact | Shubham Tyagi's Blog" });
 })
+
+
+
 
 module.exports = router;
