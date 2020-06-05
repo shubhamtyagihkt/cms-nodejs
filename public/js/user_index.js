825c16e2970c60386a1ce86d10360c21f16d2070
nitinpal07 <nitinluck0123@gmail.com>
2020-06-05 21:01:00 +0530

    single post api call

--- public/js/user_index.js
+++ public/js/user_index.js
@@ -14,32 +14,37 @@ function showPosts(posts){
 		var post = allPosts[i];
 		//console.log(post);
 
-			
+			var currentDate = new Date(post.created_on);
+
+			var date = currentDate.getDate();
+			var month = currentDate.getMonth(); //Be careful! January is 0 not 1
+			var year = currentDate.getFullYear();
+
+			var dateString = date + "-" +(month + 1) + "-" + year;
 			postlist+=
 			`
 			<li>
 			<div class="col-md-12">
 				<div class="blog-entry ftco-animate d-md-flex">
-					<a href="single.html" class="img img-2" style="background-image: url(`+post.image_url+`);"></a>
+					<a href="single" class="img img-2" style="background-image: url(`+post.image_url+`);"></a>
 						<div class="text text-2 pl-md-4">
-							<h3 class="mb-2"><a href="single.html">`+post.title+`</a></h3>
+							<h3 class="mb-2">`+post.title+`</h3>
 								<div class="meta-wrap">
 									<p class="meta">
-									<span><i class="icon-calendar mr-2"></i>`+post.created_on+`</span>
+									<span><i class="icon-calendar mr-2"></i>`+dateString+`</span>
 									<span><a href="single.html"><i class="icon-folder-o mr-2"></i>`+post.category_id+`</a></span>
 									<span><i class="icon-comment2 mr-2"></i>5 Comment</span>
 									</p>
 								</div>
 							<p class="mb-4">`+post.description+`</p>
-							<p><a href="#" class="btn-custom">Read More <span class="ion-ios-arrow-forward"></span></a></p>
+							<p><a href="single" onclick="return showSinglePost(`+post.post_id+`);"class="btn-custom">Read More <span class="ion-ios-arrow-forward"></span></a></p>
 						</div>
 				</div>
 			</div>
 			</li>
 			`;
 	}
-	postContainer.innerHTML = postlist;
-	
+	postContainer.innerHTML = postlist;	
 }
 
 
