a59824c0e9fc184f2f87c72cfb81f0897e7a7e8c
nitinpal07 <nitinluck0123@gmail.com>
2020-06-05 21:08:10 +0530

    change user-single

---
+++ public/js/user_single.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+	getSinglePost(2);
+};
+
+
+function showSinglePost(post){
+	var singlepost = document.getElementById("single-post");
+	
+	var postlist="";
+
+			postlist+=`
+							<h1 class="mb-3">`+post[0].title+`</h1>
+							<p>`+post[0].description+`</p>
+							<p>
+								<img src="`+post[0].image_url+`" alt="" class="img-fluid">
+							</p>
+							<p>`+post[0].description+`</p>
+			`;
+
+	singlepost.innerHTML = postlist;	
+
+}
+
+function getSinglePost(post_id){
+	$.ajax({
+		type: "POST",
+		contentType: "application/json",
+		data: JSON.stringify({"id":post_id}),
+		url: "http://localhost:3000/user_api_call/getSinglePost",
+		success: function(response) {
+			if(response.status == "success") {
+				showSinglePost(response.items);
+			}
+			else {
+				console.log(response);
+			}
+		},
+		error: function(xhr, status, err) {
+			console.log(err.toString());
+		}
+	});
+}
