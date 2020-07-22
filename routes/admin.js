01019a18c7152cc61559fc54ac73b658c838fa21
nitinpal07 <nitinluck0123@gmail.com>
2020-07-22 14:45:57 +0530

    changed title

--- routes/admin.js
+++ routes/admin.js
@@ -2,28 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/login', function(req, res, next) {
-  res.render('admin/admin-login', { title: "Contact | Shubham Tyagi's Blog" ,layout:'admin-layout'});
+  res.render('admin/admin-login', { title: "Contact | CMS" ,layout:'admin-layout'});
 });
 router.get('/pages', function(req, res, next) {
-  res.render('admin/admin-pages', { title: "Contact | Shubham Tyagi's Blog" ,layout:'admin-layout'});
+  res.render('admin/admin-pages', { title: "Contact | CMS" ,layout:'admin-layout'});
 });
 router.get('/users', function(req, res, next) {
-  res.render('admin/admin-users', { title: "Contact | Shubham Tyagi's Blog" ,layout:'admin-layout'});
+  res.render('admin/admin-users', { title: "Contact | CMS" ,layout:'admin-layout'});
 });
 router.get('/posts', function(req, res, next) {
-  res.render('admin/admin-posts', { title: "Contact | Shubham Tyagi's Blog" ,layout:'admin-layout'});
+  res.render('admin/admin-posts', { title: "Contact | CMS" ,layout:'admin-layout'});
 });
 
 router.get('/edit', function(req, res, next) {
-  res.render('admin/admin-edit', { title: "Contact | Shubham Tyagi's Blog" ,layout:'admin-layout',id: req.query.post_id});
+  res.render('admin/admin-edit', { title: "Contact | CMS" ,layout:'admin-layout',id: req.query.post_id});
 });
 
 router.get('/index', function(req, res, next) {
-  res.render('admin/admin-index', { title: "Contact | Shubham Tyagi's Blog" ,layout:'admin-layout'});
+  res.render('admin/admin-index', { title: "Contact | CMS" ,layout:'admin-layout'});
 });
 
 router.get('/add', function(req, res, next) {
-  res.render('admin/admin-add', { title: "Contact | Shubham Tyagi's Blog" ,layout:'admin-layout'});
+  res.render('admin/admin-add', { title: "Contact | CMS" ,layout:'admin-layout'});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('users/index', { title: "Shubham Tyagi's Blog" });
+  res.render('users/index', { title: " Blog" });
 });
 
 router.get('/travel', function(req, res, next) {
-  res.render('users/travel', { title: "Travel | Shubham Tyagi's Blog" });
+  res.render('users/travel', { title: "Travel | Blog" });
 });
 router.get('/fashion', function(req, res, next) {
-  res.render('users/fashion', { title: "Fasthion | Shubham Tyagi's Blog" });
+  res.render('users/fashion', { title: "Fashion | Blog" });
 });
 router.get('/about', function(req, res, next) {
-  res.render('users/about', { title: "About | Shubham Tyagi's Blog" });
+  res.render('users/about', { title: "About | Blog" });
 });
 router.get('/contact', function(req, res, next) {
-  res.render('users/contact', { title: "Contact | Shubham Tyagi's Blog" });
+  res.render('users/contact', { title: "Contact | Blog" });
 });
 router.get('/single', function(req, res, next) {
-  res.render('users/single', { title: "Contact | Shubham Tyagi's Blog",id: req.query.post_id});
+  res.render('users/single', { title: "Contact | Blog",id: req.query.post_id});
 });
 
 
